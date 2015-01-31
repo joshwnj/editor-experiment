731c257c2c1cadb78b408897fa4514344d58a5fa
Josh Johnston <josh@x-team.com>
2015-01-31 15:31:15 -0700

    added test command and tape

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 tape('Make a selection', function (t) {
   t.plan(1);
 
-  var selectCmd = __dirname + '/../bin/range.js';
+  var selectCmd = __dirname + '/../bin/select.js';
   var sourceFile = __dirname + '/helloworld.txt';
   var cmd = selectCmd + ' ' + sourceFile + ' -r "2 /i/ e .|/t/ e .4"';
   exec(cmd, function (err, stdout, stderr) {

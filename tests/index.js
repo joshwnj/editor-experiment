41317144bbe1e47937007f99cd68ab78a301d274
Josh Johnston <josh@x-team.com>
2015-01-31 13:54:17 -0700

    added basic test

---
+++ tests/index.js
@@ -0,0 +1,16 @@
+var tape = require('tape');
+var exec = require('child_process').exec;
+
+tape('Make a selection', function (t) {
+  t.plan(1);
+
+  var selectCmd = __dirname + '/../bin/range.js';
+  var sourceFile = __dirname + '/helloworld.txt';
+  var cmd = selectCmd + ' ' + sourceFile + ' -r "2 /i/ e .|/t/ e .4"';
+  exec(cmd, function (err, stdout, stderr) {
+    if (err) { throw err; }
+    if (stderr) { throw stderr; }
+
+    t.equals(stdout, '18 46\n', 'Range offsets are correct');
+  });
+});
